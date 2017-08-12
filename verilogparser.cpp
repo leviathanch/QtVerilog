644d5657476721bb5ad75c9746fe94ffda4fa426
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-13 03:48:54 +0800

    adding parser object

---
+++ verilogparser.cpp
@@ -0,0 +1,6 @@
+#include "verilogparser.h"
+
+VerilogParser::VerilogParser()
+{
+
+}
---
+++ verilogparser.h
@@ -0,0 +1,12 @@
+#ifndef VERILOGPARSER_H
+#define VERILOGPARSER_H
+
+#include <QObject>
+
+class VerilogParser
+{
+public:
+	VerilogParser();
+};
+
+#endif // VERILOGPARSER_H
