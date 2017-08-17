a090c7060cae957ee45219dde179558766a0287d
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-18 00:31:04 +0800

    adding test verilog files

---
+++ counter.v
@@ -0,0 +1,16 @@
+module counter (
+out, // Output of the counter
+enable, // enable for counter
+clk, // clock Input
+reset // reset Input
+);
+	output [7:0] out;
+	input enable, clk, reset;
+	reg [7:0] out;
+	always @(posedge clk)
+		if (reset) begin
+			out <= 8'b0 ;
+		end else if (enable) begin
+			out <= out + 1;
+		end
+endmodule
