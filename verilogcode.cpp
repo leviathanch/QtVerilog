0690439b0db93bf74b718dd320bc50f45bd85d73
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-18 02:22:23 +0800

    fixed missing initialization

--- verilogcode.cpp
+++ verilogcode.cpp
@@ -10,25 +10,36 @@ namespace yy {
 	class VerilogScanner;
 	class VerilogParser;
 
-	VerilogCode::VerilogCode() {}
+	VerilogCode::VerilogCode() {
+		yy_verilog_source_tree = NULL;
+		yy_preproc = NULL;
+		verilog_parser_init();
+	}
 
 	bool VerilogCode::parse_file(QString filename)
 	{
 		bool ret;
+		std::string stdfilename = filename.toStdString();
+		trace_scanning = false;
+		trace_parsing = false;
+
 		streamname = filename;
 		std::filebuf fb;
-		if (fb.open (filename.toStdString(),std::ios::in)) {
+		std::cout << "opening file " << stdfilename << std::endl;
+		if(fb.open(stdfilename,std::ios::in)) {
+			std::cout << "opened file" << stdfilename << std::endl;
 			std::istream is(&fb);
 
-			lexer = new VerilogScanner(&is);
+			lexer = new VerilogScanner(&is,&std::cout);
 			lexer->set_debug(trace_scanning);
 
 			parser = new VerilogParser(this);
 			parser->set_debug_level(trace_parsing);
 
 			ret=parser->parse();
+			fb.close();
+			std::cout << "done parsing" << std::endl;
 		}
-		std::cout << "done parsing" << std::endl;
 		return ret;
 	}
 
@@ -52,24 +63,22 @@ namespace yy {
 		for(unsigned int i = 0; i < t->items; i ++) {
 			ast_source_item* toadd = (ast_source_item*)ast_list_get(t, i);
 			if(toadd->type == SOURCE_MODULE) {
-				ast_list_append(this->yy_verilog_source_tree -> modules, toadd->module);
+				std::cout << "adding module" << std::endl;
+				ast_list_append(this->yy_verilog_source_tree->modules, toadd->module);
 			} else if (toadd -> type == SOURCE_UDP) {
 				ast_list_append(this->yy_verilog_source_tree->primitives, toadd->udp);
 			} else {
 				// Do nothing / unknown / unsupported type.
-				printf("line %d of %s - Unknown source item type: %d",
-				__LINE__,
-				__FILE__,
-				toadd -> type);
+				std::cout << "line " << __LINE__ << " of " << __FILE__" - Unknown source item type: " << toadd->type << std::endl;
 			}
 		}
 	}
 
 	void VerilogCode::showData()
 	{
+		ast_module_declaration * module;
 		verilog_source_tree *st = yy_verilog_source_tree;
-		ast_list* m = st->modules;
-		for(ast_list_element *e=m->head;e;e=e->next) {
+		for(ast_list_element * m=st->modules->head;m;m=m->next) {
 		}
 	}
 }
